561afd300f1e230c33254a8bd2e5747fff2af92b
Meraj <merajahmed2112@gmail.com>
2022-02-23 19:01:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState,useEffect} from 'react';
+import axios from 'axios';
+import 'antd/dist/antd.css'; 
+import {Layout,Table,Input} from 'antd';
+const {Content } = Layout;
+const { Search } = Input;
 
 function App() {
+
+  const [allRepo, setallRepo] = useState([]);
+
+  const [searchText, setsearchText] = useState('m879');
+
+  const columns = [
+    {
+      title: `Repository Name ( ${searchText} )`,
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Star',
+      dataIndex: 'stargazers_count',
+      key: 'stargazers_count',
+    }
+  ];
+
+  useEffect(() => {
+    if(searchText){
+      axios.get(`https://api.github.com/users/${searchText}/repos`)
+      .then((res)=>{
+        setallRepo(res.data);
+      }).catch((err)=>{
+        console.log(err);
+      })
+    }else{
+      setallRepo([]);
+      setsearchText('m879')
+    }
+  }, [searchText]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+    <Content style={{ padding: '50px' }}>
+      <div style={{margin:'10px 0px'}}>
+          <Search placeholder="GitHub Username" onChange={(e)=>setsearchText(e.target.value)} 
+          style={{ width: '50vw' }} />
+      </div>
+      <div className="site-layout-content">
+      <Table dataSource={allRepo} columns={columns} />
+      </div>
+    </Content>
+  </Layout>
   );
 }
 
